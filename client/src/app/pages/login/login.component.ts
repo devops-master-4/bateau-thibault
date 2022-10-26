2a7ad4deec7e5fa77efe9ba8febe1749d5ea2d9e
WAMANR <adil.rouichi011@gmail.com>
2022-10-26 22:53:01 +0200

    adqeqfzqefe

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +21,12 @@ export class LoginComponent implements OnInit {
     if(!email.value || !password.value){
       alert('Veuillez remplir tous les champs');
     }
+    else{
+      //Si la connection est valide
+      this.router.navigate(["/home"])
+    }
+    
+    
   }
 
 }
