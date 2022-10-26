d64f4af39cbb296f027a0c3febdd8620f5ec61ff
WAMANR <adil.rouichi011@gmail.com>
2022-10-26 16:20:13 +0200

    add login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DetailsProduitsComponent } from './pages/details-produits/details-produits.component';
 import {HomeComponent} from "./pages/home/home.component";
 import {DonneesHistoriqueComponent} from "./pages/donnees-historique/donnees-historique.component";
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent,},
+  { path: '', component: LoginComponent,},
   { path: 'home', component:HomeComponent},
   { path: 'detailsProduit', component:DetailsProduitsComponent},
   { path: 'donnesHistorique', component:DonneesHistoriqueComponent},
+  { path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
