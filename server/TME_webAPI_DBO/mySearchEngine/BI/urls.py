baced22282b9e5c9a6406b7b3fb0fc1c0ff683f8
WAMANR <adil.rouichi011@gmail.com>
2022-10-28 00:16:03 +0200

    BI backend

--- server/TME_webAPI_DBO/mySearchEngine/BI/urls.py
+++ server/TME_webAPI_DBO/mySearchEngine/BI/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-import transaction.views as views 
+import BI.views as views
+
 
 urlpatterns = [
-        path('testBI/', views.AddTransaction.as_view()),
+        path('BI/CA', views.getChiffreAffaire.as_view()),
+        path('BI/benef', views.getBenefice.as_view()),
+        path('BI/impot', views.getTaxes.as_view()),
+        path('BI/stock', views.getStock.as_view()),
+        path('BI/price', views.getPrice.as_view()),
+        path('BI/sell', views.getSell.as_view()),
+        path('BI/buy', views.getBuy.as_view()),
+        path('BI/lost', views.getLost.as_view()),
 ]
