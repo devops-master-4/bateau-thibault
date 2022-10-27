f97f8d7d2402553c1046bc9f07eaa1afe581edb8
meteor314 <meteor3141592@gmail.com>
2022-10-27 11:28:24 +0200

    login in back

--- server/TME_webAPI_DBO/mySearchEngine/account/urls.py
+++ server/TME_webAPI_DBO/mySearchEngine/account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from account import views
 
 urlpatterns = [
+    path('login/', views.LoginView.as_view()),
+    path('isConnected/', views.isConnected.as_view()),
 
 ]
--- server/TME_webAPI_DBO/mySearchEngine/account/views.py
+++ server/TME_webAPI_DBO/mySearchEngine/account/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .models import User
+import jwt
+import datetime
 
-# Create your views here.
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        if not email or not password:
+            raise AuthenticationFailed('Please provide email and password')
+        if email == "admin@admin.com" and password == "admin" :
+        
+            payload = {
+                'id': 1,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+                'iat': datetime.datetime.utcnow()
+                }
+            token = jwt.encode(payload, 'secret', algorithm='HS256')
+            response = Response()
+            response.set_cookie(key='auth-token', value=token, httponly=True)
+            response.data = {
+                'jwt': token
+            }
+            return response
+        else:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        
+
+class isConnected(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('auth-token')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        try :
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated')
+        user = payload['id']
+        return Response({
+            'id': user
+        })
+
+
+
+
+
+
