2217b1b772fc58b348c189d4fa69467ba3b934f2
meteor314 <meteor3141592@gmail.com>
2022-10-28 12:29:01 +0200

    update commit

--- client/src/app/pages/donnees-historique/donnees-historique.component.ts
+++ client/src/app/pages/donnees-historique/donnees-historique.component.ts
@@ -6,72 +6,191 @@ import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-donnees-historique',
   templateUrl: './donnees-historique.component.html',
-  styleUrls: ['./donnees-historique.component.css']
+  styleUrls: ['./donnees-historique.component.css'],
 })
 export class donnesHistoriqueComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.createChart();
   }
   public chart: any;
   public chart2: any;
+  public chart3: any;
 
-  createChart(){
-  
-    this.chart = new Chart("MyChart", {
-      type: 'bar', //this denotes tha type of chart
+  createChart() {
+    // get data from database
+    var dataSets: any = [];
+    const data: any = fetch('http://localhost:8000/BI/CA', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dataSets.push({
+          label: data.map((item: any) => item[5]),
+          price: data.map((item: any) => item[4]),
+        });
+        localStorage.setItem(
+          'price',
+          JSON.stringify(data.map((item: any) => item[4]))
+        );
+        localStorage.setItem(
+          'label',
+          JSON.stringify(data.map((item: any) => item[5]))
+        );
+        return data;
+      });
 
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-	       datasets: [
+    console.log('dataSets', dataSets);
+
+    const price = JSON.parse(localStorage.getItem('price') || '{}');
+    const label = JSON.parse(localStorage.getItem('label') || '{}');
+
+    // print first json object
+    console.log('label', price[0]);
+    this.chart = new Chart('MyChart', {
+      type: 'line',
+      data: {
+        labels: label,
+        datasets: [
           {
-            label: "Sales",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'blue'
+            label: label,
+            data: price,
+
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: ['rgba(255, 99, 132, 1)'],
           },
-          {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
-        ]
+        ],
       },
       options: {
-        aspectRatio:2.5
-      }
-      
-    });  
-    this.chart2 = new Chart("MyChart2", {
-      type: 'bar', //this denotes tha type of chart
-
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-	       datasets: [
-          {
-            label: "Sales",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'blue'
+        scales: {
+          y: {
+            beginAtZero: true,
           },
-          {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
-        ]
+        },
       },
-      options: {
-        aspectRatio:2.5
-      }
-      
     });
-  }
 
+    const benef: any = fetch('http://localhost:8000/BI/benef', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dataSets.push({
+          label: data.map((item: any) => item[5]),
+          price: data.map((item: any) => item[4]),
+        });
+        localStorage.setItem(
+          'priceBenf',
+          JSON.stringify(data.map((item: any) => item[4]))
+        );
+        localStorage.setItem(
+          'labelBenf',
+          JSON.stringify(data.map((item: any) => item[5]))
+        );
+        return data;
+      });
+    
+
+    const priceBenf = JSON.parse(localStorage.getItem('priceBenf') || '{}');
+    const labelBenf = JSON.parse(localStorage.getItem('labelBenf') || '{}');
+    this.chart2 = new Chart('MyChart2', {
+      type: 'line',
+      data: {
+        labels: labelBenf,
+        datasets: [
+          {
+            label: label,
+            data: priceBenf,
+
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: ['rgba(255, 99, 132, 1)'],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+
+    const impot = fetch('http://localhost:8000/BI/impot', {
+      method: 'GET',
+
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => { 
+        dataSets.push({
+          label: data.map((item: any) => item[5]),
+          price: data.map((item: any) => item[4]),
+        });
+        localStorage.setItem(
+          'priceImpot',
+          JSON.stringify(data.map((item: any) => item[0]))
+        );
+        localStorage.setItem(
+          'labelImpot',
+          JSON.stringify(data.map((item: any) => item[1]))
+        );
+        return data;
+      }
+
+      );
+
+    let priceImpot = JSON.parse(localStorage.getItem('priceImpot') || '{}');
+    const labelImpot = JSON.parse(localStorage.getItem('labelImpot') || '{}');
+    (priceImpot == 0) ? priceImpot = 100 : priceImpot = priceImpot;
+    console.log('priceImpot', priceImpot);
+    this.chart3 = new Chart('MyChart3', {
+      type: 'doughnut',
+      data: {
+        labels: labelBenf,
+        datasets: [
+          {
+            label: labelImpot,
+            data: [
+              priceImpot,
+            ],
+            backgroundColor: [
+              'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: ['rgba(255, 99, 132, 1)'],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+  }
 }
